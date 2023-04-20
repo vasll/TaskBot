d4cf2718b574b027e83099c0ec610873ae2356f0
vasll <valentinosoll17@gmail.com>
2023-04-20 19:22:07 +0200

    Moved utils to single file

--- utils/gmt_timezones.py
+++
@@ -1,4 +0,0 @@
-gmt_timezones = ['Etc/GMT+0', 'Etc/GMT+1', 'Etc/GMT+2', 'Etc/GMT+3', 'Etc/GMT+4', 'Etc/GMT+5', 'Etc/GMT+6', 'Etc/GMT+7',
-                 'Etc/GMT+8', 'Etc/GMT+9', 'Etc/GMT+10', 'Etc/GMT+11', 'Etc/GMT+12', 'Etc/GMT-1', 'Etc/GMT-2',
-                 'Etc/GMT-3', 'Etc/GMT-4', 'Etc/GMT-5', 'Etc/GMT-6', 'Etc/GMT-7', 'Etc/GMT-8', 'Etc/GMT-9',
-                 'Etc/GMT-10', 'Etc/GMT-11']
